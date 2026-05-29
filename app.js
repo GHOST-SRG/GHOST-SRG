2c26f8f6969e49192c31eec89ae12cd201551ed3
GHOST-SRG <jaxongirvd@gmail.com>
2026-05-29 17:36:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+setTimeout(() => {
+  const splash = document.getElementById("splash");
+
+  splash.style.opacity = "0";
+  splash.style.transition = "0.5s ease";
+
+  setTimeout(() => {
+    splash.style.display = "none";
+  }, 600);
+
+}, 3500);
